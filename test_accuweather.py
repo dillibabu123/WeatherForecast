e48b92acb8129bc0d9d7d2c1e990223f94aaf0f4
dillibabu123 <55104669+dillibabu123@users.noreply.github.com>
2022-05-25 22:47:30 +0530

    Update test_accuweather.py

--- test_accuweather.py
+++ test_accuweather.py
@@ -26,12 +26,9 @@ receiver_email = "dillibabu.aragonda@Dell.com"  # Enter receiver address
 password = "qmevstitvzxdmwli"
 
 message = """\
-<<<<<<< HEAD
+
 Subject: Bangalore Weather forecast from Dilli Python code
-=======
-Subject: Bangalore Weather forecast from Dilli Python code
->>>>>>> 37ffc3b9349be36e2e05b7a3b79c634435071824
-To: Sumanjit.Saha@Dell.com
+To: dillibabu.aragonda@Dell.com
 
 Bengaluru, Karnataka Weather forecast from Accuweather.com. This mail is sent from Python code.\n
 Current Date: """ + date + """\n
